19eb807d800cf07748fdf775442e5eba74c9ba40
Alan Fendrich <7389921+afendrich@users.noreply.github.com>
2018-05-09 16:29:15 -0400

    Add files via upload

---
+++ javascript/designs.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+  $('.project1').on('click', function(e) {
+    e.preventDefault();
+    console.log('project1 clicked');
+    window.location="https://codepen.io/alan1234/full/bMLvym/"
+    return false;
+  });
+  $('.project2').on('click', function(e) {
+    e.preventDefault();
+    console.log('project1 clicked');
+    window.location="https://codepen.io/alan1234/full/WJpqEj/"
+    return false;
+  });
+  $('.project3').on('click', function(e) {
+    e.preventDefault();
+    console.log('project1 clicked');
+    window.location="https://codepen.io/alan1234/full/NYJoeV/"
+    return false;
+  });
+
+});
